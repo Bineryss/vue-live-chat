ef4176916d49ab40c3732b78650d40c6947d9311
Sebastian Otto <44194014+Bineryss@users.noreply.github.com>
2022-02-07 20:31:19 +0100

    init

---
+++ src/views/Welcome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="welcome container">
+    <p>Welcome</p>
+    <SignupForm/>
+  </div>
+</template>
+
+<script>
+import SignupForm from "@/components/SignupForm.vue";
+
+export default {
+  components: {SignupForm}
+}
+</script>
+
+<style scoped>
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
+</style>
